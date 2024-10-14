dc0fb2ef5d9cdf6f41fb6d601b94a3c6134809cd
CoolJoel <joelastrom.op@gmail.com>
2024-10-14 16:57:19 +0200

    Added BIM file

---
+++ Assignment 2/BIM.py
@@ -0,0 +1,12 @@
+BIM = [
+        #Name           |        Functionality
+    
+      ['tc_relay',          ['send','configure'] ],
+      ['houskeeping',    ['on','off','configure']]
+      ['mode',                ['check', 'change']]
+      ['attitude',               ['get','change']]
+      ['star_tracker',   ['on','off','configure']]
+      ['scheduele',   ['command','clear','check']]
+      ['battery_kill',                  'command']
+      
+      ]
